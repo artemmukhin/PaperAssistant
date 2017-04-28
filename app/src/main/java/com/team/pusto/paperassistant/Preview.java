0142e6e3b0de2a743e4a9c7a139cc896a3ac1501
Snyssfx <snyssfx@gmail.com>
2017-04-28 18:20:43 +0300

    add animation and usecases

--- app/src/main/java/com/team/pusto/paperassistant/Preview.java
+++ app/src/main/java/com/team/pusto/paperassistant/Preview.java
@@ -1,15 +1,29 @@
 package com.team.pusto.paperassistant;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-/**
- * Created by fearl on 28.04.2017.
- */
+public class Preview extends AppCompatActivity {
 
-class Preview extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.preview);
+        setContentView(R.layout.activity_preview);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
 }
