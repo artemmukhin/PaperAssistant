f20212f54075d2ca625a41c57f8aef816236f314
Snyssfx <snyssfx@gmail.com>
2017-04-29 12:30:46 +0300

    final commit

--- app/src/main/java/com/team/pusto/paperassistant/Gallery.java
+++ app/src/main/java/com/team/pusto/paperassistant/Gallery.java
@@ -26,6 +26,8 @@ import android.widget.Toast;
 
 import com.team.pusto.paperassistant.classifierengine.Classifier;
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
